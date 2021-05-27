d27c25a90dab909cbf4d52e2b6f199798bff5d14
ayaishi1213 <shitianxuannai958@gmail.com>
2021-05-27 22:12:19 +0900

    createアクション実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,9 @@ class PostsController < ApplicationController
   def index
     @posts = Post.order(id: "DESC")
   end
-
+  
+  def create
+    Post.creat(params[:id])
+    redirect_to action: :index
+  end
 end
