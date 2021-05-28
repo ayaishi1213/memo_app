105c089e53dfba37159d3a57c06c0fa326ed97bc
ayaishi1213 <shitianxuannai958@gmail.com>
2021-05-28 13:11:53 +0900

    JavaScriptファイル作成

---
+++ app/javascript/memo.js
@@ -0,0 +1,15 @@
+const { formDisableSelector } = require("@rails/ujs");
+
+function post (){
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", () => {
+    const form =document.getElementById("form")
+    const formData = new formDate(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
+};
+
+window.addEventListener('load', post);
